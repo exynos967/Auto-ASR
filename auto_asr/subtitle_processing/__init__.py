49c1f35bc1f1adc159518089a1531efd340262e9
xiaoxi68 <3520824673@qq.com>
2026-01-29 22:54:42 +0800

    feat: auto-register built-in subtitle processors

--- auto_asr/subtitle_processing/__init__.py
+++ auto_asr/subtitle_processing/__init__.py
@@ -1,4 +1,6 @@
 from .base import SubtitleProcessor, get_processor, list_processors, register_processor
 
-__all__ = ["SubtitleProcessor", "get_processor", "list_processors", "register_processor"]
+# Import built-in processors for registration side effects (OCP-friendly plugin registry).
+from . import processors as _processors  # noqa: F401
 
+__all__ = ["SubtitleProcessor", "get_processor", "list_processors", "register_processor"]
